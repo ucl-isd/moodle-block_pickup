099db65ad8a5dc0e8bb154c4a838ed59542b1c34
Alistair Spark <aspark21@users.noreply.github.com>
2024-12-14 07:42:49 +0000

    Version bump for 4.5

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023072700;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022112800;        // Requires this Moodle version.
+$plugin->version   = 2024121800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2024100700;        // Requires this Moodle version.
 $plugin->release   = '1.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'block_pickup';    // Full name of the plugin (used for diagnostics).
